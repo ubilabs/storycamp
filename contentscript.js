df5fa24370ddca7fd9ae0c46c1201773f20a1e27
Martin Kleppe <kleppe@ubilabs.net>
2011-02-15 13:59:33 +0100

    Textile markup added.

--- contentscript.js
+++ contentscript.js
@@ -1,15 +1,17 @@
 function load_items(){
-  var $body = $("body").html("");
+  var $body = $("body").html(""); 
+   
   $("link").remove();
   
   var style = "body { font-family: Arial; }" +
-    "div.story { height: 13.7cm; font-size: 1cm; padding-bottom: 1em; page-break-before: always; font-weight: bold; }" +
-    "div.citeria { border-top: 1px solid #000; font-size: 0.5cm; white-space: pre; line-height: 1.5em; height: 13.7cm; position: relative; }" +
-    "div.citeria .content{ -webkit-transform: rotate(180deg); padding-bottom: 1cm; }";
+    "ul { padding: 0 0 0 1em; } " +
+    "ul, li { margin: 0; }" +
+    "div.story { font-size: 20px; padding-bottom: 1em; page-break-before: always; font-weight: bold; }" +
+    "div.citeria { font-size: 14px !important; line-height: 1.2em; font-weight: normal; margin-top: 0.5em;}";
   
   $body.append('<style type="text/css" media="all">' + style + '</style>');
   
-  $.get('/projects/4071498/todo_lists/11815962.xml', function(xml){
+  $.get(document.location.pathname + '.xml', function(xml){
     
     var $items = $("todo-item", xml),
       count = $items.length;
@@ -22,12 +24,18 @@ function load_items(){
         
       $div.append(title);
       
-      $div = $("<div class='citeria'>").appendTo($body);
+      $div = $("<div class='citeria'>").appendTo($div);
 
       $.get("/todo_items/" + id + "/comments.xml", function(xml){
         var comment = $("comment body", xml).text();
-        comment = comment.replace("* ", "<br />* ");
-        $div.html("<div class='content'>" + comment + "</div>");
+        
+        comment = convert(comment);
+                
+        if (comment.indexOf("<li>") === 0){
+          comment = "<ul>" + comment + "</ul>";
+        }
+                
+        $div.html(comment);
         
         if (!--count){ print(); }
       });
